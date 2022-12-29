22f2c6d7080ebc0352e95e129ba8f1bc3342be3f
rinchetra <rinchetra06@gmail.com>
2022-12-30 03:15:25 +0700

    upaded ui widget layout

--- lib/ui/view/home_view.dart
+++ lib/ui/view/home_view.dart
@@ -91,10 +91,132 @@ class _HomeViewState extends State<HomeView> {
                   ),
                 ),
                 SizedBox(
-                  width: 10,
+                  width: 15,
                 ),
               ],
             ),
+            SingleChildScrollView(
+              child: Row(
+                children: [
+                  Image.network(
+                      width: MediaQuery.of(context).size.width * 0.5,
+                      height: 150,
+                      fit: BoxFit.cover,
+                      "https://benstore.com.ph/28962-pdt_771/asus-rog-zephyrus-g14-ga402rj-advantage-edition-14-gaming-laptop-120hz-ryzen-9-6900hs-radeon-rx-6700s-16gb-ddr5-1tb-ssd-.jpg"),
+                  Image.network(
+                      width: MediaQuery.of(context).size.width * 0.4,
+                      height: 140,
+                      fit: BoxFit.cover,
+                      "https://i.gadgets360cdn.com/large/dell_xps_13_plus_dell_1641381925034.jpg"),
+                ],
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: Container(
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5),
+                      color: Colors.white,
+                    ),
+                    child: Column(
+                      children: [
+                        Icon(
+                          Icons.time_to_leave,
+                          color: Colors.red,
+                        ),
+                        Text("Today's Deal"),
+                        SizedBox(
+                          height: 5,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Expanded(
+                  child: Container(
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5),
+                      color: Colors.yellow,
+                    ),
+                    child: Column(
+                      children: [
+                        Icon(Icons.upload_file_sharp),
+                        Text("Flash Sale"),
+                        SizedBox(
+                          height: 5,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 15,
+                ),
+                Expanded(
+                  child: Container(
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5),
+                      color: Colors.white,
+                    ),
+                    child: Column(
+                      children: [
+                        Icon(Icons.scale_sharp),
+                        Text("Flash Sale"),
+                        SizedBox(
+                          height: 5,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                //   SizedBox(
+                //   width: 15,
+                // ),
+              ],
+            ),
+
+            SingleChildScrollView(
+              child:Row(
+                children: [
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.45,
+                    decoration: BoxDecoration(borderRadius: BorderRadius.circular(5),color:Colors.white),
+                    child: ListTile(
+                      leading: Icon(Icons.picture_as_pdf),
+                      title: Text("Women clothes"),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.40,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5),
+                        color: Colors.white),
+                    child: ListTile(
+                      leading: Icon(Icons.picture_as_pdf),
+                      title: Text("Women clothes"),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 8,
+                  )
+                ],
+              ),
+
+
+            )
+            
+      
           ],
         ),
       ),
